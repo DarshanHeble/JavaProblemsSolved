fee277dc107ad9ad8c7d5f44ba1edf1045bf6581
DarshanHeble <109078053+DarshanHeble@users.noreply.github.com>
2023-06-29 18:44:34 +0530

    .

--- Arrays/Medium/BestTimeBuySell.java
+++ Arrays/Medium/BestTimeBuySell.java
@@ -19,7 +19,27 @@
 // Explanation: In this case, no transactions are done and the max profit = 0.
 
 public class BestTimeBuySell {
+    public static int maxProfit(int prices[]) {
+        int maxProfit = 0, profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] < prices[j]) {
+                    profit = prices[j] - prices[i];
+                }
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        if (maxProfit = 0) {
+            return 0;
+        } else {
+            return maxProfit;
+        }
+    }
+
     public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
+        maxProfit(prices);
     }
 }
