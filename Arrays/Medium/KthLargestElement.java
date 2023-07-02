be7dc63dc61a2db0042e1a64efc29165a6c5fe0b
DarshanHeble <109078053+DarshanHeble@users.noreply.github.com>
2023-07-02 23:57:06 +0530

    .

--- Arrays/Medium/KthLargestElement.java
+++ Arrays/Medium/KthLargestElement.java
@@ -15,6 +15,29 @@
 // Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
 // Output: 4
 
-public class KthLargestElement {
+import java.util.*;
 
+public class KthLargestElement {
+    public static int kthLargest(int nums[], int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // adding k elements to min heap
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (pq.peek() < nums[i]) {
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 3, 2, 1, 5, 6, 4 }, k = 3;
+
+        System.out.println(kthLargest(nums, k));
+    }
 }
