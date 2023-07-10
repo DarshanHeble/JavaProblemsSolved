7f35c53b1f66511308769d13deeee1a52dd819ae
DarshanHeble <109078053+DarshanHeble@users.noreply.github.com>
2023-07-11 01:01:27 +0530

    wrote 2 programs

--- Arrays/Easy/Containsdup.java
+++ Arrays/Easy/Containsdup.java
@@ -1,6 +1,7 @@
 // Contains Duplicate
 
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Given an integer array nums, return true if any value appears at least twice in the array,
+//  and return false if every element is distinct.
 
 // Example 1:
 
@@ -15,6 +16,30 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-public class Containsdup {
+// import a HashSet class into a program 
+import java.util.HashSet;
 
+public class Containsdup {
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 1 };
+        System.out.println(checkDuplicates(a));
+    }
+
+    // create a boolean function so that you can return true and false
+    public static boolean checkDuplicates(int a[]) {
+        // create a hashSet object
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < a.length; i++) {
+            // check if a[0] value is present in the hashSet
+            if (hs.contains(a[i])) {
+                // if yes return true
+                return true;
+            } else {
+                // else add to hashSet
+                hs.add(a[i]);
+            }
+        }
+        // return false if there is no Duplicates
+        return false;
+    }
 }
--- Arrays/Easy/reverseArr.java
+++ Arrays/Easy/reverseArr.java
@@ -11,6 +11,43 @@
 
 public class reverseArr {
     public static void main(String[] args) {
+        int[] a = { 1, 2, 3 };
+        reverseArrUsingWhile(a, 0, a.length - 1);
+        reverseArrUsingfor(a, a.length - 1);
+    }
 
+    // reversing an array using while loop
+    public static void reverseArrUsingWhile(int a[], int first, int last) {
+        // swap until first is smaller then last
+        while (first < last) {
+            // swap 1st and last
+            int temp = a[first];
+            a[first] = a[last];
+            a[last] = temp;
+
+            first++;
+            last--;
+        }
+        // print the array
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    // reversing an array using for loop
+    public static void reverseArrUsingfor(int a[], int last) {
+        // swap until half a array
+        for (int i = 0; i < a.length / 2; i++) {
+            // swaping
+            int temp = a[i];
+            a[i] = a[last];
+            a[last] = temp;
+
+            last--;
+        }
+        // print the array
+        for (int k = 0; k < a.length; k++) {
+            System.out.println(a[k]);
+        }
     }
 }
